c1bdf22b7335ac4c2d149e0f90a34177e280d796
Salah Uddin @Sweden <77015008+SalahUddinSweden@users.noreply.github.com>
2021-04-29 01:20:58 +0200

    Add files via upload

---
+++ Lesson 9- Practice session for Task 3. Addition and Sub.  Anis IB/index.js
@@ -0,0 +1,41 @@
+
+//Test
+
+// var num1 = prompt('Enter first number: ');
+// var num2 = prompt('Enter first number: ');
+
+// num1= parseInt(num1,10);
+// num2= parseInt(num2,10);
+
+// var sum,sub;
+// sum= num1 + num2;
+
+// document.write('ADDITION =' + sum + '<br/>');
+
+// sub= num1-num2;
+// document.write('SUBTRACTION =' + sub);
+
+
+//Task 3 Done by Salah Uddin
+
+var num1= prompt('Enter first Number: ');
+var num2= prompt('Enter second Number: ');
+
+num1= parseInt(num1,10);
+num2= parseInt(num2,10);
+
+sum = num1 +num2;
+document.write('Addition =' + sum + '<br/>');
+
+sub = num1 - num2;
+document.write('Subtraction =' + sub + '<br/>');
+
+
+mult = num1 * num2;
+document.write('Multiply =' + mult + '<br/>');
+
+div = num1 / num2;
+document.write('Divide =' + div + '<br/>');
+
+modulus = num1 % num2;
+document.write('Modulus =' + modulus + '<br/>');
